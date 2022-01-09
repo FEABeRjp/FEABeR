a7d20223c05112bc11dbc0b219290a1417fd9af2
Tetsuo Koyama <tkoyama010@gmail.com>
2022-01-09 15:53:52 +0900

    Fix by black

--- getfem-cooks-membrane.py
+++ getfem-cooks-membrane.py
@@ -7,6 +7,8 @@ import getfem as gf
 import numpy as np
 import pyvista as pv
 
+pv.set_plot_theme("document")
+
 ###############################################################################
 
 mesh = gf.Mesh("regular simplices", range(5), range(5))
@@ -14,7 +16,7 @@ pts = mesh.pts()
 x = mesh.pts()[0, :]
 y = mesh.pts()[1, :]
 pts[0, :] = x * 44.0 / 4
-pts[1, :] = 44.0 / 4 * x + (44.0 - (44.0 - 16.0) / 4 * x ) / 4 * y
+pts[1, :] = 44.0 / 4 * x + (44.0 - (44.0 - 16.0) / 4 * x) / 4 * y
 mesh.set_pts(pts)
 mesh.export_to_vtk("mesh.vtk", "ascii")
 
@@ -22,6 +24,7 @@ grid = pv.read("mesh.vtk")
 points = grid.points
 plotter = pv.Plotter(off_screen=True)
 plotter.add_mesh(grid, show_edges=True)
-plotter.add_point_labels(points, points.tolist())
+plotter.add_point_labels(
+    points, points.tolist(), point_size=10, font_size=10, always_visible=True
+)
 plotter.show(cpos="xy", screenshot="mesh.png")
-
